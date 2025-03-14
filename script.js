33250ce05a9e2c1737f90afa5589e2833c41eb49
Dacio Azevedo <dacioherazan2@gmail.com>
2025-03-14 13:44:59 -0300

    Adicionando estilos aos botões

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(id) {
-    const humanChoice = id;
-    if (humanChoice === "rock") {
+    if (id === "rock") {
         return "rock";
-    } else if (humanChoice === "paper") {
+    } else if (id === "paper") {
         return "paper";
-    } else if (humanChoice === "scissors") {
+    } else if (id === "scissors") {
         return "scissors";
     }
 }
@@ -25,6 +24,7 @@ let computerScore = 0;
 
 function playGame(event) {
     let results = document.querySelector("#results");
+    let matchResult = document.querySelector("#matchResult");
     const buttonId = event.target.id;
 
     const humanChoice = getHumanChoice(buttonId);
@@ -32,23 +32,30 @@ function playGame(event) {
 
     if (humanChoice === "rock" && computerChoice === "scissors") {
         humanScore++;
+        matchResult.textContent = "You won! Rock beats Scissors";
         console.log("You won! Rock beats Scissors");
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         humanScore++;
+        matchResult.textContent = "You won! Scissors beats Paper";
         console.log("You won! Scissors beats Paper");
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         humanScore++;
+        matchResult.textContent = "You won! Paper beats Rock";
         console.log("You won! Paper beats Rock");
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         computerScore++;
+        matchResult.textContent = "You lost! Scissors beats Paper";
         console.log("You lost! Scissors beats Paper");
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         computerScore++;
+        matchResult.textContent = "You lost! Paper beats Rock";
         console.log("You lost! Paper beats Rock");
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         computerScore++;
+        matchResult.textContent = "You lost! Rock beats Scissors";
         console.log("You lost! Rock beats Scissors");
     } else if (humanChoice === computerChoice) {
+        matchResult.textContent = "Draw!";
         console.log("Draw!");
     }
 
